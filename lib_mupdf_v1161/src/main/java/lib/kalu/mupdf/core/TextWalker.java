bb6949d222735e26b21de5aa98b6232c5bdd9e83
Administrator <153437803@qq.com>
2021-01-13 08:53:51 +0800

    fix file

--- lib_mupdf_v1161/src/main/java/lib/kalu/mupdf/core/TextWalker.java
+++
@@ -1,8 +0,0 @@
-package lib.kalu.mupdf.core;
-
-import androidx.annotation.Keep;
-
-@Keep
-public interface TextWalker {
-    void showGlyph(Font font, Matrix trm, int glyph, int unicode, boolean wmode);
-}
