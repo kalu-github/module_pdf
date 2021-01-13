c01ff2e68abf4eec58ca65bcfb19338a2f6407f8
Administrator <153437803@qq.com>
2021-01-13 14:06:50 +0800

    delte

--- lib_mupdf/src/main/cpp/platform/wasm/wrap.js
+++
@@ -1,2 +0,0 @@
-Module.noExitRuntime = true;
-Module.noInitialRun = true;
--- lib_mupdf/src/main/cpp/source/fitz/z-imp.h
+++
@@ -1,9 +0,0 @@
-#ifndef FITZ_Z_IMP_H
-#define FITZ_Z_IMP_H
-
-#include <zlib.h>
-
-void *fz_zlib_alloc(void *ctx, unsigned int items, unsigned int size);
-void fz_zlib_free(void *ctx, void *ptr);
-
-#endif
